fda89f0e0990816c312d3c2f8a5aeebf9e999010
liront212 <lirontabib212@gmail.com>
2024-04-10 20:21:33 +0300

    add new file

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Dynamic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.ExceptionServices;
+using System.IO;
+using static cyberAcademy.cyberAcademy;
+
+namespace cyberAcademy
+{
+    public class cyberAcademy
+    {
+        public class Student
+        {
+            public string FirstName
+            {
+                get { return FirstName; ; }
+                set { FirstName = value; }
+            }
+            public string LastName
+            {
+                get { return LastName; ; }
+                set { LastName = value; }
+            }
+            public string _StudentID
+            {
+                get { return _StudentID; ; }
+                set { _StudentID = value; }
+            }
+            public string _DateOfBirth
+            {
+                get { return _DateOfBirth; ; }
+                set { _DateOfBirth = value; }
+            }
+            public string _MaritalStatus
+            {
+                get { return _MaritalStatus; ; }
+                set { _MaritalStatus = value; }
+            }
+            public Student(string FirstName, string LastName, string _StudentID, string _DateOfBirth, string _MaritalStatus)
+            {
+                this.FirstName = FirstName;
+                this.LastName = LastName;
+                this._StudentID = _StudentID;
+                this._DateOfBirth= _DateOfBirth;
+                this._MaritalStatus= _MaritalStatus;
+            }
+        }        
+
+    }
+    
+}
+
+
+
+
---
+++ main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static cyberAcademy.cyberAcademy;
+
+namespace cyberAcademy
+{
+    class Student
+    {
+
+        public static void Main(string[] args)
+        {
+            Student student = new Student();
+            Console.WriteLine($"Enter your first name:");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine($"Enter your last name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"Enter your ID:");
+            string _StudentID = Console.ReadLine();
+            Console.WriteLine($"Enter your DateOfBirth:");
+            string _DateOfBirth = Console.ReadLine();
+            Console.WriteLine($"Enter your MaritalStatus:");
+            string _MaritalStatus = Console.ReadLine();
+        }
+    }
+}
